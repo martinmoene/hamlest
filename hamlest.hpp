cd5a422d3e6f99318cb88189fe5926f4632f7999
Martin Moene <martin.moene@gmail.com>
2013-09-03 09:55:43 +0200

    Add sequence matchers

--- hamlest.hpp
+++ hamlest.hpp
@@ -58,17 +58,23 @@ using Matcher = std::shared_ptr<matcher<T>>;
  * matcher for values of type T.
  */
 template <typename T>
-struct matcher // : public std::unary_function<T, bool> // has non-virtual destructor
+struct matcher
 {
     using argument_type = T;
-    
+    using value_type = T;
+
     virtual ~matcher() { }
 
     /// matcher copy
     virtual Matcher<T> copy() const = 0;
 
-    /// true if matches
-    virtual bool operator()( T const & actual ) const = 0;
+    /// true if matches for value
+    virtual bool operator()( T const & /*actual*/ ) const { return false; };
+//    virtual bool operator()( T const & actual ) const = 0;
+
+    /// true if matches for container
+    template <typename C>
+    bool operator()( C const & /*cont*/ ) const { return false; };
 
     /// matcher description using expectation values.
     virtual std::string to_string() const = 0;
@@ -151,23 +157,6 @@ struct close_to_matcher : matcher<T>
 
     const T expected;
     const T delta;
-};
-
-/**
- * anything( [description] ): match anything of type T.
- */
-template <typename T>
-struct anything : matcher<T>
-{
-    explicit anything( std::string description = "[anything]" ) : description( description ) { }
-
-    Matcher<T> copy() const { return Matcher<T>( new anything(*this) ); }
-
-    bool operator()( T const & /*actual*/ ) const { return true; }
-
-    std::string to_string() const { return lest::to_string( description ); }
-
-    const std::string description;
 };
 
 /**
@@ -268,11 +257,159 @@ struct match_regexp_matcher : public matcher<std::string>
 };
 
 /**
+ * contains( element ).
+ */
+template <typename T>
+struct contains_element_matcher : public matcher<T>
+{
+    explicit contains_element_matcher( T const & element ) : element( element ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new contains_element_matcher( *this ) ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const { return end(cont) != std::find( begin(cont), end(cont), element ); }
+
+    std::string to_string() const { return "contains " + lest::to_string( element ); }
+
+    const T element;
+};
+
+/**
+ * contains( matcher ).
+ */
+template <typename T>
+struct contains_matcher : matcher<T>
+{
+    explicit contains_matcher( matcher<T> const & match ) : match( match.copy() ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new contains_matcher( *this)  ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const 
+    { 
+        return std::any_of(
+            cont.begin(), cont.end(),
+            [this]( typename C::value_type const & value ) { return (*match)(value); }
+        );
+    }
+
+    std::string to_string() const { return "contains " +  match->to_string(); }
+    
+    Matcher<T> match;
+};
+
+/**
+ * contains( sequence ).
+ */
+template <typename T>
+struct contains_sequence_matcher : public matcher<T>
+{
+    explicit contains_sequence_matcher( std::initializer_list<T> const & sequence ) : sequence( sequence ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new contains_sequence_matcher( *this ) ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const
+    {
+        return cont.end() != std::search( cont.begin(), cont.end(), sequence.begin(), sequence.end() );
+    }
+
+    std::string to_string() const { return "contains " + lest::to_string( sequence ); }
+
+    const std::initializer_list<T> sequence;
+};
+
+/**
+ * contains_elements( list ).
+ */
+template <typename T>
+struct contains_elements_matcher : public matcher<T>
+{
+    explicit contains_elements_matcher( std::initializer_list<T> const & list ) : list( list ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new contains_elements_matcher( *this ) ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const
+    {
+#if 1
+        return ! std::any_of( begin(list), end(list)
+            , [&cont]( typename C::value_type const & value ) { return 0 == std::count( begin(cont), end(cont), value ); } );
+#else
+        for ( auto const & element : list )
+        {
+            if ( 0 == std::count( begin(cont), end(cont), element ) )
+                return false;
+        }
+        return true;
+#endif
+    }
+
+    std::string to_string() const { return "contains elements " + lest::to_string( list ); }
+
+    const std::initializer_list<T> list;
+};
+
+/**
+ * is_empty().
+ */
+struct is_empty_matcher : matcher<int>
+{
+    explicit is_empty_matcher() { }
+
+    Matcher<int> copy() const { return Matcher<int>( new is_empty_matcher() ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const { return cont.empty(); }
+
+    std::string to_string() const { return "is empty "; }
+};
+
+/**
+ * size_is().
+ */
+template <typename T>
+struct size_is_matcher : matcher<T>
+{
+    size_is_matcher( T const & size ) : match( equal_to_matcher<T>( size ).copy() ) { }
+
+    explicit size_is_matcher( matcher<T> const & match ) : match( match.copy() ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new size_is_matcher( *this)  ); }
+
+    template <typename C>
+    bool operator()( C const & cont ) const { return (*match)( cont.size() ); }
+
+    std::string to_string() const { return "size " +  match->to_string(); }
+    
+    Matcher<T> match;
+};
+
+/**
+ * anything( [description] ): match anything of type T.
+ */
+template <typename T>
+struct anything : matcher<T>
+{
+    explicit anything( std::string description = "[anything]" ) : description( description ) { }
+
+    Matcher<T> copy() const { return Matcher<T>( new anything(*this) ); }
+
+    bool operator()( T const & /*actual*/ ) const { return true; }
+
+    std::string to_string() const { return lest::to_string( description ); }
+
+    const std::string description;
+};
+
+/**
  * is( matcher ), is( value )
  */
 template <typename T>
 struct is_matcher : matcher<T>
 {
+    is_matcher( T value ) : match( equal_to_matcher<T>( value ).copy() ) { }
+
     explicit is_matcher( matcher<T> const & match ) : match( match.copy() ) { }
 
     Matcher<T> copy() const { return match; }
@@ -290,6 +427,8 @@ struct is_matcher : matcher<T>
 template <typename T>
 struct is_not_matcher : matcher<T>
 {
+    is_not_matcher( T value ) : match( equal_to_matcher<T>( value ).copy() ) { }
+
     explicit is_not_matcher( matcher<T> const & match ) : match( match.copy() ) { }
 
     Matcher<T> copy() const { return match; }
@@ -423,6 +562,14 @@ LEST_TAG_MATCHER( is_not_matcher );
 LEST_TAG_MATCHER( any_of_matcher );
 LEST_TAG_MATCHER( all_of_matcher );
 
+// distinguish between matchers and values:
+
+template <typename T, typename R>
+using ForMatcher = typename std::enable_if< std::is_class<T>::value, R>::type;
+
+template <typename T, typename R>
+using ForValue = typename std::enable_if< ! std::is_class<T>::value, R>::type;
+
 // matcher funtions to resolve types:
 
 template <typename T>
@@ -524,14 +671,60 @@ inline contains_string_matcher contains( std::string const & text )
     return contains_string_matcher( text );
 }
 
-inline contains_regexp_matcher contains_regexp( std::string const & expr )
+inline contains_string_matcher contains( const char * text )
+{
+    return contains_string_matcher( text );
+}
+
+inline auto contains_regexp( std::string const & expr ) -> contains_regexp_matcher 
 {
     return contains_regexp_matcher( expr );
 }
 
-inline match_regexp_matcher matches_regexp( std::string const & expr )
+inline auto matches_regexp( std::string const & expr ) -> match_regexp_matcher 
 {
     return match_regexp_matcher( expr );
+}
+
+template <typename T>
+inline auto contains( T const & element ) -> ForMatcher<T, contains_matcher<typename T::argument_type>>
+{
+    return contains_matcher<typename T::argument_type>( element );
+}
+
+template <typename T>
+inline auto contains( T const & element ) -> ForValue<T, contains_element_matcher<T>>
+{
+    return contains_element_matcher<T>( element );
+}
+
+template <typename T>
+inline auto contains( std::initializer_list<T> const & list ) -> contains_sequence_matcher<T> 
+{
+    return contains_sequence_matcher<T>( list );
+}
+
+template <typename T>
+inline auto contains_elements( std::initializer_list<T> const & list ) -> contains_elements_matcher<T> 
+{
+    return contains_elements_matcher<T>( list );
+}
+
+inline auto is_empty() -> is_empty_matcher 
+{
+    return is_empty_matcher();
+}
+
+template <typename T>
+inline auto size_is( T const & match ) -> ForValue<T, size_is_matcher<std::size_t>>
+{
+    return size_is_matcher<std::size_t>( match );
+}
+
+template <typename M>
+inline auto size_is( M const & match ) -> ForMatcher<M, size_is_matcher<typename M::value_type>>
+{
+    return size_is_matcher<typename M::value_type>( match );
 }
 
 template <typename T>
@@ -543,7 +736,7 @@ inline is_matcher<typename T::argument_type> is_implementation( T const & expect
 template <typename T>
 inline is_matcher<T> is_implementation( T const & expected, not_matcher_tag )
 {
-    return is_matcher<T>( equal_to( expected ) );
+    return is_matcher<T>( expected );
 }
 
 template <typename T>
@@ -567,7 +760,8 @@ inline is_not_matcher<typename T::argument_type> is_not_implementation( T const 
 template <typename T>
 inline is_not_matcher<T> is_not_implementation( T const & expected, not_matcher_tag )
 {
-    return is_not_matcher<T>( equal_to( expected ) );
+    return is_not_matcher<T>( expected );
+//    return is_not_matcher<T>( equal_to( expected ) );
 }
 
 template <typename T>
@@ -635,9 +829,10 @@ using lest::matches_regexp;
 
 // sequence:
 
-//using lest::contains;
-//using lest::contains_in_any_order;
-//using lest::has_items;
+//using lest::contains;         // already declared
+using lest::contains_elements;
+using lest::is_empty;
+using lest::size_is;
 
 // dictionary:
 
@@ -646,7 +841,10 @@ using lest::matches_regexp;
 //using lest::has_key;
 //using lest::has_value;
 
-// logical: 
+// logical:
+
+//template <typename T> using a = lest::anything<T>;
+//template <typename T> using an = lest::anything<T>;
 
 using lest::anything;
 
